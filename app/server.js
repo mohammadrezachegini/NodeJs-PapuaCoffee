d31f6e3f7f846f3dc1dc730518b20067d771b38c
Reza <mohammadreza.chegini01@gmail.com>
2023-03-25 09:57:09 -0700

    initial configs are done

--- app/server.js
+++ app/server.js
@@ -26,18 +26,91 @@ module,exports = class Application {
         this.InitRedis();
     }
 
-    configApplication(){}
+    configApplication(){
+        this.#app.use(cors());
+        this.#app.use(morgan('dev'));
+        this.#app.use(express.json());
+        this.#app.use(express.urlencoded({extended: true}))
+        this.#app.use(express.static(path.join(__dirname, "..", "public")));
+        this.#app.use("/api-doc", swaggerUI.serve, swaggerUI.setup(swaggerJsDoc({
+            swaggerDefinition: {
+                info: {
+                    title: "Papua Coffee",
+                    version: "2.0.0",
+                    description: "The biggest store around the world",
+                    contact: {
+                        name: "Reza Chegini",
+                        url: "https://papuaCoffee.com",
+                        email: "mr.goodarzvand.chegini@gmail.com"
+                    }
 
-    createServer(){}
+                },
+                servers: [
+                    {
+                        url: "http://localhost:5000"
+                    }
+                ]
 
-    connectToMongoose(){}
+            },
+            apis:["./app/routers/**/*.js"]
+        })))
+    }
 
-    InitRedis(){}
+    createServer(){
+        const httpp = require('http');
+        httpp.createServer(this.#app).listen(this.#PORT, () => {
+            console.log(`Server is running on port ${this.#PORT}`);
+        })
+    }
+
+    connectToMongoose(){
+        mongoose.set("strictQuery", true)
+        mongoose.connect(this.#DB_URI).then(()=>{
+            console.log("Connect to DB is Successful");
+        }).catch((e)=>{
+            console.log("No connection " + e);
+        });
+
+        mongoose.connection.on("connected" , () => {
+            console.log("mongoose connected to db");
+        })
+        mongoose.connection.on("disconnected", () => {
+            console.log("mongoose connection is disconnected");
+        })
+
+        process.on("SIGINT", async() => {
+            console.log("CLOSED");
+            await mongoose.connection.close()
+            process.exit(0)
+        })
+    }
+
+    InitRedis(){
+        require("../utils/init_redis")
+    }
 
 
-    createRoutes(){}
+    createRoutes(){
+        this.#app.use(AllRoutes)
+    }
 
-    errorHandling(){}
+    errorHandling(){
+        this.#app.use((req,res,next) => {
+            next(createError.NotFound("Page not found"))
+        })
+        this.#app.use((error, req,res,next)=> {
+            const serverError = createError.InternalServerError()
+            const statusCode = error.status || serverError.status;
+            const message = error.message || serverError.message
+            return res.status(statusCode).json({
+                errors: {
+                    statusCode,
+                    message
+                }
+            })
+        })
+    }
+    }
 
 
 
