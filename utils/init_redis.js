2a5480700e65ce19f0c060301e1ae2ab4cc0604f
Reza <mohammadreza.chegini01@gmail.com>
2023-04-01 16:48:05 -0700

    change redis server5

--- utils/init_redis.js
+++ utils/init_redis.js
@@ -1,5 +1,5 @@
 const redisDB = require('redis');
-const redisClient = redisDB.createClient({{
+const redisClient = redisDB.createClient({
     password: 'a19941994M@@',
     socket: {
         host: 'redis-13106.c61.us-east-1-3.ec2.cloud.redislabs.com',
